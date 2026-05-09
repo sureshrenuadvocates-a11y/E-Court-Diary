a5727cf4097f3c18e173dbd4961fae62463bf0de
sureshrenuadvocates-a11y <sureshrenuadvocates@gmail.com>
2026-05-09 02:57:13 -0600

    Update EmailVerificationSystem.js

--- backend/Utlis/EmailVerificationSystem.js
+++ backend/Utlis/EmailVerificationSystem.js
@@ -1,46 +1,26 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER || 'armandhull5540@gmail.com',
+    pass: process.env.EMAIL_PASS || 'ohpjncbkxclkttzu',
+  },
+});
 
 export const sendOTPEmail = async (email, otp) => {
-  try {
-
-    const response = await resend.emails.send({
-      from: "E-Court Diary <onboarding@resend.dev>",
-
-      to: email,
-
-      subject: "Email Verification OTP",
-
-      html: `
-        <div style="font-family: Arial; padding: 20px;">
-          <h1>E-Court Diary</h1>
-
-          <h2>Your OTP:</h2>
-
-          <div style="
-            font-size: 32px;
-            font-weight: bold;
-            color: blue;
-          ">
-            ${otp}
-          </div>
-
-          <p>This OTP expires in 10 minutes.</p>
-        </div>
-      `,
-    });
-
-    console.log("Email sent:");
-    console.log(response);
-
-    return true;
-
-  } catch (error) {
-
-    console.log("EMAIL ERROR:");
-    console.log(error);
-
-    return false;
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Email Verification OTP for E-Court Diary",
+    html: `
+  <h1 style="color: #4A90E2;">E-Court Diary Email Verification</h1>
+      <p>Dear User,</p>
+      <p>Thank you for registering with E-Court Diary. To complete your registration and verify your email address, please use the following One-Time Password (OTP):</p>
+      <h2>Your OTP Code</h2>
+      <p>${otp}</p>
+      <p>This OTP will expire in 10 minutes.</p>
+    `,
+  });
 };
