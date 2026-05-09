e7be9aaa6cf73eb596fa2afdcf08410ec1e00eaa
sureshrenuadvocates-a11y <sureshrenuadvocates@gmail.com>
2026-05-09 03:06:22 -0600

    Update EmailVerificationSystem.js

--- backend/Utlis/EmailVerificationSystem.js
+++ backend/Utlis/EmailVerificationSystem.js
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 
-
 const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER || 'armandhull5540@gmail.com',
     pass: process.env.EMAIL_PASS || 'ohpjncbkxclkttzu',
@@ -10,17 +11,20 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendOTPEmail = async (email, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Email Verification OTP for E-Court Diary",
-    html: `
-  <h1 style="color: #4A90E2;">E-Court Diary Email Verification</h1>
-      <p>Dear User,</p>
-      <p>Thank you for registering with E-Court Diary. To complete your registration and verify your email address, please use the following One-Time Password (OTP):</p>
-      <h2>Your OTP Code</h2>
-      <p>${otp}</p>
-      <p>This OTP will expire in 10 minutes.</p>
-    `,
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: `"E-Court Diary" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Email Verification OTP",
+      html: `
+        <h1>E-Court Diary</h1>
+        <h2>${otp}</h2>
+      `,
+    });
+
+    console.log(info);
+
+  } catch (error) {
+    console.log(error);
+  }
 };
