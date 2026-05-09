43db62e345a7fd909631c71d7614bef8cdb9e38f
sureshrenuadvocates-a11y <sureshrenuadvocates@gmail.com>
2026-05-09 03:02:01 -0600

    Update EmailVerificationSystem.js

--- backend/Utlis/EmailVerificationSystem.js
+++ backend/Utlis/EmailVerificationSystem.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
   auth: {
     user: process.env.EMAIL_USER || 'armandhull5540@gmail.com',
     pass: process.env.EMAIL_PASS || 'ohpjncbkxclkttzu',
